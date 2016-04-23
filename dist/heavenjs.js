f500805122c3bf7cbb1552585fbf474088fa4ad6
Quesha-Lania <rachman.sd@gmail.com>
2016-04-23 09:18:22 +0800

    contributor

--- dist/heavenjs.js
+++ dist/heavenjs.js
@@ -11,7 +11,7 @@
     }
 })(window,jQuery, function ($) {
     var heavenJS = function (GLOBAL) {
-        var controller, model={},GLOBALS={syBegin:"\\(\\[",syEnd:"//]//)"};
+        var controller, storage={}, model={},GLOBALS={syBegin:"\\(\\[",syEnd:"//]//)"};
         if(typeof GLOBAL !== 'undefined' && typeof GLOBAL.control !== 'undefined'){
             controller = GLOBAL.control;
             delete GLOBAL.control;
@@ -33,7 +33,13 @@
         };
         this.getGlobal=function(){
             return GLOBALS;
-        }
+        };
+        this.setStorage=function(s,v){
+            storage[s]=v;
+        };
+        this.getStorage= function () {
+            return storage;
+        };
     };
     heavenJS.prototype.control= function (control) {
         this.setController(control);
@@ -42,23 +48,38 @@
     heavenJS.prototype.model= function (obj) {
         console.log(obj(this.getModel()));
     };
-    heavenJS.prototype.foreach= function (object, model) {
-        var html;
+    heavenJS.prototype.foreach= function (obj, model) {
+        var html, modelUri;
         var getMatch= function (str,flags) {
-            var symbol;
+            var symbol, Expression, result=null;
             symbol=this.getGlobal().syBegin;
             symbol +="[_a-zA-Z0-9.]+|[a-z.++]+|[a-z.]+\\[\\d+\\]\\+|math\\[[+-/*_a-zA-Z0-9.]+\\]|math\\[[+-/*_a-zA-Z0-9.]+\\]\\([-_a-zA-Z0-9.]+\\)|[a-zA-Z.]+\\[[_a-zA-Z0-9.]+\\]|[a-zA-Z.]+\\[[_a-zA-Z0-9.]+\\]\\([#-:_a-zA-Z0-9.]+\\)";
             symbol +=this.getGlobal().syEnd;
-            
+            Expression = typeof flags != 'undefined' ?
+                new RegExp(symbol) :
+                new RegExp(symbol);
+            if(typeof str != 'undefined'){
+                result= str.match(Expression);
+            }
+            return result;
         };
-
-        if((/[_a-zA-Z0-9]+ as [_a-zA-Z0-9]+/).test(model)){
-            var model = model.match(/([_a-zA-Z0-9]+) as ([_a-zA-Z0-9]+)/),setModel={},getModel=model[2];
-            setModel[getModel]= function () {}; this.setModel(setModel); this.getModel()[getModel](getModel);
-        }else{
-            html=this.getController().find('*[model="'+model+'"]').html();
-            console.log(html);
+        if(typeof obj != 'undefined' && typeof model != 'undefined'){
+            if((/[_a-zA-Z0-9]+ as [_a-zA-Z0-9]+/).test(model)){
+                var model = model.match(/([_a-zA-Z0-9]+) as ([_a-zA-Z0-9]+)/),setModel={},getModel=model[2];
+                setModel[getModel]= function () {}; this.setModel(setModel); this.getModel()[getModel](getModel);
+            }else{
+                modelUri=this.getController().find('*[model="'+model+'"]'); html=modelUri.html();
+                if(typeof this.getStorage()[model] != 'undefined'){
+                    html = this.getStorage()[model];
+                }else{
+                    this.setStorage(model,html);
+                }
+                console.log(html);
+                
+            }
         }
+    };
+    heavenJS.prototype.pagination= function () {
 
     };
     heavenJS.prototype.check= function () {
