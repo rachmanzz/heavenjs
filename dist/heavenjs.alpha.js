f1c50aeaa94e00182f001b3a3ca5454e92e12993
Rahman <rachman.sd@gmail.com>
2017-01-29 21:32:11 +0800

    update new version

--- dist/heavenjs.alpha.js
+++ dist/heavenjs.alpha.js
@@ -23,11 +23,31 @@
   isFunc = function(v){
     return typeof v === 'function'
   },
+  // check if undefined
   isUndef= function(v){
     return typeof v === 'undefined'
   },
   isNull = function(v){
     return v == 'null';
+  },
+  // merge object
+  mObject = function(obj,obj1){
+      for(var key in obj){
+          if(obj1.hasOwnProperty(key) &&
+              isObject(obj[key]) && isObject(obj1[key]))
+              mObject(obj[key],obj1[key]);
+          else obj1[key] = obj[key];
+      }
+      return obj1;
+  },
+  // extract array
+  each = function (l,call) {
+      if(isArray(l)){
+        var i=0;
+        for(i; i < l.length; ++i){
+            call(i,l[i]);
+        }
+      } else console.log("it's not an array [each]")
   }
 
 
